fcc39faac4646e57f30465824e801935cfd8136c
Brett Heap <Brett.Heap@Opensoft.one>
2025-04-16 03:11:35 -0700

    Update run and test container files

--- .runcontainer/entrypoint.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-exec dotnet Nop.Web.dll
--- .testcontainer/stacks/DES/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Add .devcontainer to PATH
-export PATH="/workspace/.devcontainer:$PATH"
-
-# keep the container running
-echo "Container is running, development tools ready..."
-echo "You can use the \"clone\" command from any nopCommerce directory to clone plugins."
-tail -f /dev/null
